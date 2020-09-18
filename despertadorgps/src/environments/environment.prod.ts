0a601c97cef3bc9132365b8000503bc021bafb90
Gisela Acosta <gacosta@technisys.com>
2020-09-18 08:11:16 -0300

    commit

---
+++ despertadorgps/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBQmouUNu9chf1VmjNm9sxcHXhHDRnq_cQ",
+    authDomain: "despertadorgps-e4595.firebaseapp.com",
+    databaseURL: "https://despertadorgps-e4595.firebaseio.com",
+    projectId: "despertadorgps-e4595",
+    storageBucket: "despertadorgps-e4595.appspot.com",
+    messagingSenderId: "738335742561",
+    appId: "1:738335742561:web:938b079bde314b80ed2820",
+    measurementId: "G-BL3KF757LN"
+  }
+};
---
+++ despertadorgps/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBQmouUNu9chf1VmjNm9sxcHXhHDRnq_cQ",
+    authDomain: "despertadorgps-e4595.firebaseapp.com",
+    databaseURL: "https://despertadorgps-e4595.firebaseio.com",
+    projectId: "despertadorgps-e4595",
+    storageBucket: "despertadorgps-e4595.appspot.com",
+    messagingSenderId: "738335742561",
+    appId: "1:738335742561:web:938b079bde314b80ed2820",
+    measurementId: "G-BL3KF757LN"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
